25301109bd22ad929ae6387fc043780aaed91da8
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-20 06:48:15 +0000

    First commit with freelance app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HomepageComponent }  from './homepage/homepage.component';
+import { DocumentsComponent }  from './documents/documents.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule,
+    AppRoutingModule
+    ],
+  declarations: [ 
+    AppComponent,
+    HomepageComponent,
+    DocumentsComponent
+    ],
+  bootstrap:    [ 
+    AppComponent 
+    ]
 })
 export class AppModule { }

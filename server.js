8214b9b5aff8dc90b80eea6d4ee992499fed9dd7
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-21 06:51:25 +0000

    modified server file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/src/app"));
-app.listen(process.env.PORT || 3001);
-console.log("Sever up on port 3001");
+app.use(express.static(__dirname + "/app"));
+app.listen(process.env.PORT || 8080);
+console.log("Sever up on port 8080");

f0814c3b030d905da15d966e4909f45ea15541ec
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-21 07:15:32 +0000

    update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + "/app"));
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
-console.log("Sever up on port 8080");

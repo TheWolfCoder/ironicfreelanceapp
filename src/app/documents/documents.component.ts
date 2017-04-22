5ccf08404ee45d54525245937e581c8a4fe35639
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-22 08:21:10 +0000

    Added boostrap styling to application

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
 	   	  description: "afdasdf l;akdjas;fjd", 
 	   	  file_url: "http://google.com", 
 	   	  updated_at: '11/11/16', 
-	   	  image_url: "http://google.com",
+	   	  image_url: "https://cdn.pixabay.com/photo/2014/09/24/14/29/mac-459196_960_720.jpg",
 	   	},
 	   	{ 
 	   		title: "My Second Doc", 
 	   	  description: "afdasdf l;akdjas;fjd", 
 	   	  file_url: "http://google.com", 
 	   	  updated_at: '11/11/16', 
-	   	  image_url: "http://google.com",
+	   	  image_url: "https://cdn.pixabay.com/photo/2014/12/27/15/31/camera-581126_960_720.jpg",
 	   	},
 	   	{ 
 	   		title: "My Last Doc", 
 	   	  description: "afdasdf l;akdjas;fjd", 
 	   	  file_url: "http://google.com", 
 	   	  updated_at: '11/11/16', 
-	   	  image_url: "http://google.com",
+	   	  image_url: "https://cdn.pixabay.com/photo/2015/01/01/23/04/mouse-586244_960_720.jpg",
 	   	}
 		]
 }
